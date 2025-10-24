434a6d6a42701b18772d58278a4c142c15a4c159
Arnav Agrawal <arnavagr@usc.edu>
2025-10-23 20:29:11 -0700

    changes

--- logicsim/circuit.h
+++ logicsim/circuit.h
@@ -14,16 +14,15 @@ class Circuit
 		void test();
 		bool advance(std::ostream&);
 		void run(std::ostream&);
-    bool parse(const char*);
-    void startUml(std::ostream&);
-    void endUml(std::ostream&);
+		bool parse(const char*);
+		void startUml(std::ostream&);
+		void endUml(std::ostream&);
 		
 	private:
 		uint64_t m_current_time;
-    std::vector<Gate*> m_gates;
-    std::vector<Wire*> m_wires;
-		// we need to add the m_pq data member. It should be a min-heap of Event*;
-    Heap<Event*, EventLess> m_pq;
+		std::vector<Gate*> m_gates;
+		std::vector<Wire*> m_wires;
+		Heap<Event*, EventLess> m_pq;
         
 };
 
